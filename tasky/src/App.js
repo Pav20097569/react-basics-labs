a3e2d5e0f36cf146d0f9c1ae93e04a61d9b80140
Pav20097569 <20097569@mail.wit.ie>
2024-10-01 14:01:58 +0100

    Delete A Task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -17,6 +17,14 @@ function App() {
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -28,6 +36,7 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
   ))}
     </div>
