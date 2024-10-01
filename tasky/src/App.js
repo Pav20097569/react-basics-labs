ccb862763a23e7e12042f80f6033e0c650db7962
Pav20097569 <20097569@mail.wit.ie>
2024-10-01 13:46:16 +0100

    Rendered list of tasks using map

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 function App() {
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" description ="Empty the Dishwasher and Put the dishes away" />
-      <Task title="Tidy" deadline="Today" description="Tide the House" />
-      <Task title="Laundry" deadline="Tomorrow" description="Do the laundry"/>
-        
+      {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+    />
+  ))} 
     </div>
   );
 }
